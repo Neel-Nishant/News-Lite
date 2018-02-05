09893aaec0ec1e58554cf1f327b6e4d4c855d265
Neel-Nishant <neeljvm@gmail.com>
2018-02-06 00:19:50 +0530

    Initial Commit

---
+++ News Lite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News Lite
+//
+//  Created by Neel Nishant on 06/02/18.
+//  Copyright © 2018 Neel Nishant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
